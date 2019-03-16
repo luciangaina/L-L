51869074144370801a066352462e7ff61088a351
luciangaina <43759441+luciangaina@users.noreply.github.com>
2019-03-16 22:14:27 +0200

    prototip chicken & run/jump (sapt1)
    characters, run, jump, ground

---
+++ ChickenRun/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D myRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidbody.velocity = new Vector2(moveSpeed,myRigidbody.velocity.y); //run
+
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce); //jump
+        }
+    }
+}
