b8d513c07d47184d2404e36994701b2c9fe1b294
luciangaina <43759441+luciangaina@users.noreply.github.com>
2019-03-20 22:27:29 +0200

    animations created
    start camera follow, finding ground, animations created

--- ChickenRun/Assets/Scripts/PlayerController.cs
+++ ChickenRun/Assets/Scripts/PlayerController.cs
@@ -9,20 +9,32 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D myRigidbody;
 
+    public bool grounded;
+    public LayerMask whatIsGround;
+
+    private Collider2D myCollider;
+
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+
+        myCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
         myRigidbody.velocity = new Vector2(moveSpeed,myRigidbody.velocity.y); //run
 
-        if(Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce); //jump
+            if (grounded)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce); //jump
+            }
         }
     }
 }
