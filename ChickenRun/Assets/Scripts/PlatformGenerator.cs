67d8efc0244df377dd22025ec9556840e8c42bae
luciangaina <43759441+luciangaina@users.noreply.github.com>
2019-04-18 00:34:58 +0300

    generating random platforms

--- ChickenRun/Assets/Scripts/PlatformGenerator.cs
+++ ChickenRun/Assets/Scripts/PlatformGenerator.cs
@@ -13,12 +13,23 @@ public class PlatformGenerator : MonoBehaviour
     public float distanceBetweenMin;
     public float distanceBetweenMax;
 
-    public ObjectPooler theObjectPool;
+    public GameObject[] thePlatforms;
+    private int platformSelector;
+    private float[] platformWidths;
+
+    //public ObjectPooler theObjectPool;
 
     // Start is called before the first frame update
     void Start()
     {
-        platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+        //platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+
+        platformWidths = new float[thePlatforms.Length];
+
+        for(int i=0;i<thePlatforms.Length;i++)
+        {
+            platformWidths[i] = thePlatforms[i].GetComponent<BoxCollider2D>().size.x;
+        }
     }
 
     // Update is called once per frame
@@ -28,14 +39,18 @@ public class PlatformGenerator : MonoBehaviour
         {
             distanceBetween = Random.Range (distanceBetweenMin,distanceBetweenMax);
 
-            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+            platformSelector = Random.Range(0, thePlatforms.Length);
 
-            //Instantiate (thePlatform, transform.position, transform.rotation
+            transform.position = new Vector3(transform.position.x + platformWidths[platformSelector] + distanceBetween, transform.position.y, transform.position.z);
+
+            Instantiate (/*thePlatform*/ thePlatforms[platformSelector], transform.position, transform.rotation);
+
+            /*
             GameObject newPlatform = theObjectPool.GetPooledObject();
 
             newPlatform.transform.position = transform.position;
             newPlatform.transform.rotation = transform.rotation;
-            newPlatform.SetActive(true);
+            newPlatform.SetActive(true); */
         }
     }
 }
